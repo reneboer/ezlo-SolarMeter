32d53cc826c7dbe513a46eed13f25109405c6f18
reneboer <git@reneboer.demon.nl>
2020-08-31 15:08:11 +0200

    Add files via upload

---
+++ scripts/drivers/solarman/poll.lua
@@ -0,0 +1,34 @@
+-- solarman poll handler
+-- Update needed, so poll the device.
+local function d_poll(device)
+	local storage = require("storage")
+	local PLUGIN = storage.get_string("PLUGIN")
+	local logger = require("HUB:"..PLUGIN.."/scripts/utils/log").setPrefix(PLUGIN.."/scripts/drivers/solarman/poll").setLevel(storage.get_number("log_level") or 99)
+
+	local http = require("http")
+	local URI = "https://home.solarman.cn/cpro/device/inverter/goDetailAjax.json"
+	
+	logger.debug("device: %1", device)
+
+	-- Get device details and send http request for data. update script will handle response.
+	if device then
+		local cnf = device.config
+		local request_body = "deviceId=" .. cnf.device_id
+		local headers = {
+			['origin'] = 'https://home.solarman.cn',
+			['referer'] = 'https://home.solarman.cn/device/inverter/view.html?v=2.2.9.2',
+			['accept'] = 'application/json',
+			['content-type'] = 'application/x-www-form-urlencoded',
+			['content-length'] = string.len(request_body),
+			['cookie'] = 'language=2; autoLogin=on; Language=en_US; rememberMe=' .. cnf.token
+		}
+		URI = URI:format(device.config.ip)
+		local hndlr = "HUB:"..PLUGIN.."/scripts/drivers/solarman/update"
+		logger.debug("Envoy Local URL %1, handler %2", URI, hndlr)
+		http.request { url = URI, headers = headers, type = "POST", data = request_body, handler = hndlr, user_data = device.device_id }
+	else
+		logger.err("No device specified?")
+	end
+end
+
+d_poll(...)
---
+++ scripts/drivers/solarman/update.lua
@@ -0,0 +1,45 @@
+-- SolarMeter solarman http data response handler
+local function d_update(device, data)
+	local storage = require("storage")
+	local PLUGIN = storage.get_string("PLUGIN")
+	local json = require("HUB:"..PLUGIN.."/scripts/utils/dkjson")
+	local logger = require("HUB:"..PLUGIN.."/scripts/utils/log").setPrefix(PLUGIN.."/scripts/drivers/solarman/update").setLevel(storage.get_number("log_level") or 99)
+	
+	local function GetAsNumber(value)
+		if type(value) == "number" then return value end
+		local nv = tonumber(value,10)
+		return (nv or 0)
+	end
+
+	local ts, watts, DayKWH, WeekKWH, MonthKWH, YearKWH, LifeKWH = -1,-1,-1,-1,-1,-1,-1
+	logger.debug("device %1, data %2", device, data)
+	local retData = json.decode(data)
+	if type(retData) == "table" then
+		local id = math.floor(dev.id)
+		for key, value in pairs(retData.result.deviceWapper.dataJSON) do
+			if key == "dt" then
+				ts = GetAsNumber(value) / 1000
+			elseif key == "1ab" then 		-- DC Output Total Power (Active)
+				watts = GetAsNumber(value)
+			elseif key == "1bd" then 		-- Daily Generation (Active)
+				DayKWH = GetAsNumber(value)
+				if DayKWH ~= -1 then
+					WeekKWH = loadfile("HUB:"..PLUGIN.."/scripts/utils/week_total")().total(DayKWH, id)
+				end	
+			elseif key == "1be" then 	-- Monthly Generation (Active)
+				MonthKWH = GetAsNumber(value)
+			elseif key == "1bf" then 	-- Annual Generation (Active)
+				YearKWH = GetAsNumber(value)
+			elseif key == "1bc" then 	-- Total Generation (Active)
+				LifeKWH = GetAsNumber(value)
+			end	
+		end
+		if watts == -1 then watts = 0 end	-- Not sure where to get it from, return zero rather than -1
+		return {timestamp=ts, watts=watts, DayKWH=DayKWH, WeekKWH=WeekKWH, MonthKWH=MonthKWH, YearKWH=YearKWH, LifeKWH=LifeKWH}
+	else
+		logger.warn("Unexpected body data type %1, expected a JSON string.", type(retData))
+		return nil
+	end
+end
+
+return d_update(...)
